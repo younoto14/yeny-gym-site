ab85520f060162d93f27f9afa6588352cb268215
younoto14 <boody2006gamer@gmail.com>
2026-04-27 22:27:37 +0300

    Update route.js

--- app/api/auth/route.js
+++ app/api/auth/route.js
@@ -1,1 +1,7 @@
+import { redirect } from 'next/navigation';
 
+export async function GET() {
+  const clientId = process.env.GITHUB_CLIENT_ID;
+  const redirectURL = `https://github.com/login/oauth/authorize?client_id=${clientId}&scope=repo`;
+  redirect(redirectURL);
+}
